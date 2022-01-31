170c0ed1f40cc621c252d2a5f6edb0704e1eae51
Nokwanda <noksitho021@student.wethinkcode.co.za>
2022-01-31 12:56:40 +0200

    Created a welcome message for the user, fixes #20

--- code_clinic_io/code_clinic_output.py
+++ code_clinic_io/code_clinic_output.py
@@ -2,3 +2,9 @@
 Output functions for Code Clinic Booking System
 
 """
+
+def welcome_msg():
+    """It displays a welcome message to the user.
+    """
+
+    print("Welcome to WeThinkCode's Code Clinic Booking System.")
