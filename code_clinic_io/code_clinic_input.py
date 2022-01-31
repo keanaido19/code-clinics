40e8e2324950a064816a8d9419a4dd72f3c8aedb
Nokwanda <noksitho021@student.wethinkcode.co.za>
2022-01-31 13:14:04 +0200

    Created a function which prompts the user for a username, fixes #21

--- code_clinic_io/code_clinic_input.py
+++ code_clinic_io/code_clinic_input.py
@@ -7,6 +7,15 @@ import sys
 
 def get_argument():
     """
-    Returns an argument from the command line
+    Returns an argument from the command line.
     """
     return " ".join(sys.argv[1:])
+
+
+def get_username():
+    """Prompts the user for a username.
+    """
+
+    username = input('Please enter username: ').strip()
+
+    return username
