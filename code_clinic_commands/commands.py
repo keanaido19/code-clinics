55a991ddce9119caeec1d67ec5ff66942c20b0cb
Nokwanda <noksitho021@student.wethinkcode.co.za>
2022-01-31 12:09:16 +0200

    Created the command handler function, fixes #14

--- code_clinic_commands/commands.py
+++ code_clinic_commands/commands.py
@@ -2,3 +2,18 @@
 Commands for the WTC Code Clinic Booking System.
 
 """
+
+def login():
+    pass
+
+
+def command_handler(command_arg):
+    """It handles commands from the command line arguments.
+    """
+    if command_arg == 'login':
+        login()
+    elif command_arg == 'student_calendar':
+        pass
+    elif command_arg == 'clinic_calendar':
+        pass
+    
