fae2860730fe9b183dea91bb507250e815e26f28
danmulop021 <danmulop021@student.wethinkcode.co.za>
2022-01-31 11:43:56 +0200

    Created a function to accept sys arguments. Also created a tests package, fixes #13

--- code_clinic_io/code_clinic_input.py
+++ code_clinic_io/code_clinic_input.py
@@ -2,3 +2,11 @@
 Input functions for Code Clinic Booking System.
 
 """
+import sys
+
+
+def get_argument():
+    """
+    Returns an argument from the command line
+    """
+    return " ".join(sys.argv[1:])
