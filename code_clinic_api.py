ebc7a71f4b227c00d72828bc0b660e18f5ae5ddc
mzomdube021 <mzomdube021@student.wethinkcode.co.za>
2022-02-08 10:07:16 +0200

    Moved the functionality of the display calendar command to the API module and creted helper functions, fixes #29

--- code_clinic_api.py
+++ code_clinic_api.py
@@ -3,7 +3,10 @@ Google calendar API calls.
 
 """
 
+from optparse import Values
 from googleapiclient.discovery import build
+import helpers
+import itertools
 
 
 def build_calendar_service(token_creds):
@@ -12,3 +15,42 @@ def build_calendar_service(token_creds):
     """
 
     return build('calendar', 'v3', credentials=token_creds)
+
+def get_calendar_data(calendar_service):
+    """
+    get the calendar data for a given number of days
+    """
+    
+    now, end = helpers.get_date_range(7)
+    
+    return calendar_service.events().list(
+        calendarId='primary', 
+        timeMin=now, 
+        timeMax=end,
+        singleEvents=True,
+        orderBy='startTime').execute()
+
+def get_calendar_events(calendar_data):
+    '''
+    This Function returns the events that a in a calendar
+    '''
+    calendar_events = calendar_data.get('items', [])
+    for calendar_event in calendar_events:
+        helpers.format_calendar_event(calendar_event)
+    return calendar_event
+
+def format_calendar_events(calendar_events):
+    '''
+    Formats the way that calendar events are returned to user.
+    '''
+    grouped_calendar_events = itertools.groupby(calendar_events,key= lambda x: x['start']['event_date'])
+    return {key:[value for value in values] for key,values in grouped_calendar_events}
+
+def get_formated_calendar_events(token_credentials):
+    '''
+    Retrieves the events and returns them in a formated form.
+    '''
+    calendar_service = build_calendar_service(token_credentials)
+    calendar_data = get_calendar_data(calendar_service)
+    calendar_events = get_calendar_events(calendar_data)
+    return format_calendar_events(calendar_events)
--- helpers.py
+++ helpers.py
@@ -1,3 +1,74 @@
 """
 Additional functions to assist other modules.
 """
+
+import datetime
+from pytz import timezone
+
+def get_current_utc_date():
+    """
+    Get the current date in utc format
+    """
+    return datetime.datetime.utcnow()
+
+def get_new_utc_date(days):
+    """
+    Returns current utc date plus number of specified days
+    """
+
+    return get_current_utc_date() + datetime.timedelta(days= days) 
+
+def get_date_range(days):
+    """
+    Returns a range of how many days will be displayed in the calendar 
+    """
+    start_date = get_current_utc_date().isoformat + 'Z'
+    end_date = get_new_utc_date(days).isoformat + 'Z'
+    return start_date,end_date
+
+def convert_to_local_timezone(date):
+    '''
+    Converts date to local timezone
+    '''
+    timestamp = date.timestamp()
+    utc_date = datetime.datetime.utcfromtimestamp(timestamp)
+    return timezone('UTC').localize(utc_date).astimezone(timezone('Africa/Johannesburg'))
+
+def get_calendar_event_start(calendar_event):
+    '''
+    Get the calendar event start date
+    '''
+    dt = datetime.datetime
+    start_time = calendar_event['start'].get('dateTime', calendar_event['start'].get('date'))
+    return convert_to_local_timezone(dt.fromisoformat(start_time))
+
+def get_calendar_event_end(calendar_event):
+    '''
+    Get the calendar event start date
+    '''
+    dt = datetime.datetime
+    end_time = calendar_event['end'].get('dateTime', calendar_event['end'].get('date'))
+    return convert_to_local_timezone(dt.fromisoformat(end_time))
+
+def format_calendar_event_start(calendar_event):
+    '''
+    formats the  calendar event start date
+    '''
+    event_start = get_calendar_event_start(calendar_event)
+    calendar_event['start']['event_date'] = event_start.strftime('%a %d-%b-%Y')
+    calendar_event['start']['event_time'] = event_start.strftime('%H:%M')
+
+def format_calendar_event_end(calendar_event):
+    '''
+    formats the  calendar event start date
+    '''
+    event_end = get_calendar_event_end(calendar_event)
+    calendar_event['end']['event_date'] = event_end.strftime('%a %d-%b-%Y')
+    calendar_event['end']['event_time'] = event_end.strftime('%H:%M %p')
+
+def format_calendar_event(calendar_event):
+    '''
+    formats the  calendar event start and end date
+    '''
+    format_calendar_event_start(calendar_event)
+    format_calendar_event_end(calendar_event)
